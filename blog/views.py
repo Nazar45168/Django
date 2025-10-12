aa7433190da6c22cb23324db01a0e96cb3400bfe
Nazar45168 <n0800690@gmail.com>
2025-10-12 21:00:45 +0300

    feat: added routing and HTML template for Django task 3

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("hello, django girls!")
+    return render(request, "blog/index.html")
