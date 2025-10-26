b855c107c71b4b770db3c7e426e8bc58932431ee
Nazar45168 <n0800690@gmail.com>
2025-10-26 19:09:06 +0200

    feat(forms): add PostForm, create/edit views + form template (UTF-8)

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
-from .views import home
-urlpatterns = [path("", home, name="home")]
+from .views import home, detail, post_new, post_edit
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("post/<int:pk>/", detail, name="post_detail"),
+    path("post/new/", post_new, name="post_new"),
+    path("post/<int:pk>/edit/", post_edit, name="post_edit"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
+from .models import Post
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form, 'mode': 'new'})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form, 'mode': 'edit', 'post': post})
 
 def home(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.order_by('-id')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
